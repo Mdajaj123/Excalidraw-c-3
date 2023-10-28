f38aa46981ad4af72ae3d4a397015aac8e1308a7
unknown <mdijaz2596@gmail.com>
2023-10-28 16:28:19 +0530

    version-1

--- drawing.js
+++ drawing.js
@@ -107,17 +107,3 @@ function drawLine(currentPosition) {
     c.lineTo(currentPosition.x, currentPosition.y);
     c.stroke();
 }
-const undo = document.getElementById("undo");
-undo.addEventListener("click", () => {
-    if (pathCount) {
-        drawingHistory.pop();
-        pathCount--;
-        if (pathCount === 0) {
-            c.clearRect(0, 0, canvas.width, canvas.height);
-
-        }
-        else {
-            c.putImageData(drawingHistory[pathCount - 1], 0, 0);
-        }
-    }
-});
---
+++ undoRedo.js
@@ -0,0 +1,14 @@
+// const undo = document.getElementById("undo");
+// undo.addEventListener("click", () => {
+//     if (pathCount) {
+//         drawingHistory.pop();
+//         pathCount--;
+//         if (pathCount === 0) {
+//             c.clearRect(0, 0, canvas.width, canvas.height);
+
+//         }
+//         else {
+//             c.putImageData(drawingHistory[pathCount - 1], 0, 0);
+//         }
+//     }
+// });
